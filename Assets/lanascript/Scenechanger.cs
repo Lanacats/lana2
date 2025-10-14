447057678acac6ca61af86b46acd3d2aa1765b20
Lanacats <Pouncer1911@gmail.com>
2025-10-13 23:18:03 -0400

    tried to add button.

--- Assets/lanascript/Scenechanger.cs
+++ Assets/lanascript/Scenechanger.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Scenechanger : MonoBehaviour
+public class SceneTransition : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Tooltip("Name of the scene to load.")]
+    public string nextSceneName;
+
+    [Tooltip("Optional delay before loading the scene (in seconds).")]
+    public float delay = 0f;
+
+    // Call this method to trigger the scene transition
+    public void LoadNextScene()
     {
-        
+        if (delay > 0f)
+        {
+            Invoke(nameof(DoSceneLoad), delay);
+        }
+        else
+        {
+            DoSceneLoad();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DoSceneLoad()
     {
-        
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("SceneTransition: Next scene name is not set.");
+        }
     }
 }
